cda7558a9dafda8133f7237f029dd4534b18f631
Nishan Singh <146644160+Nishan109@users.noreply.github.com>
2025-05-15 14:56:55 +0530

    Update site-footer.tsx

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -45,11 +45,7 @@ export function SiteFooter() {
                   Contact
                 </Link>
               </li>
-              <li>
-                <Link href="/blog" className="text-sm text-gray-400 hover:text-white transition-colors">
-                  Blog
-                </Link>
-              </li>
+    
             </ul>
           </div>
 
